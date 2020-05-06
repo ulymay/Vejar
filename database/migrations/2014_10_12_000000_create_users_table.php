a9a4708c65ddc58cefb024a9993dd34355303cf8
ulymay <ulymay@gmail.com>
2020-05-06 22:13:07 +0000

    Se redirecciono a notas al registrar

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->integer('role_id')->index();
+            $table->integer('role_id')->index()->nullable();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
